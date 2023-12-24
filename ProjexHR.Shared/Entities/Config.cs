4b043adb86349b89771875ed036d89bdb4afa9ca
Siraj Fatwani <siraj@ezrlabs.com>
2023-12-24 14:53:14 +0530

    [feature] added config from appsetting environment wise

---
+++ ProjexHR.Shared/Entities/Config.cs
@@ -0,0 +1,16 @@
+namespace ProjexHR.Shared;
+
+/// <summary>
+/// Represents configuration settings for the application.
+/// </summary>
+public class Config
+{
+    public string ApplicationBasePath { get; set; }
+    public CloudSQL CloudSQL { get; set; }
+}
+
+public class CloudSQL
+{
+    public string Database { get; set; }
+    public string ConnectionString { get; set; }
+}
--- ProjexHR.WebApi/Program.cs
+++ ProjexHR.WebApi/Program.cs
@@ -1,13 +1,31 @@
+using ProjexHR.Shared;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+// Add configuration
+builder.Services.AddOptions();
+
+builder.Configuration
+       .SetBasePath(builder.Environment.ContentRootPath)
+       .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+       .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true);
+
+builder.Services.Configure<Config>(builder.Configuration.GetSection("Config"));
+var config = builder.Configuration.GetSection("Config").Get<Config>();
+
+ConnectionConfig.Database = config.CloudSQL.Database;
+ConnectionConfig.ConnectionString = config.CloudSQL.ConnectionString;
+ConnectionConfig.ApplicationBasePath = config.ApplicationBasePath;
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// App builder
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
